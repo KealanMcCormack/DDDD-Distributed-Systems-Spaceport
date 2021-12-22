c1a2d3166ccdf03efaf896bbca9c052da1cd67ad
John <odonnell88j@gmail.com>
2021-12-22 15:41:24 +0000

    client: pom and file structure

---
+++ client/src/main/java/client/Client.java
@@ -0,0 +1,26 @@
+package client;
+
+import java.text.NumberFormat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+import service.core.ClientApplication;
+import service.core.ClientInfo;
+import service.core.Quotation;
+
+/**
+ * Client for sent REST POST request for applications for clients to Space Port
+ *
+ * @author John O'Donnell
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+    nohup java -Dnogui=true -jar JMusicBot-0.3.5.jar > log.log 2>&1 &
