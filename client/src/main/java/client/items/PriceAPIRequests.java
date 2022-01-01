21509f157a66eb8be9fee26920dcd7b73d67c5ff
John <odonnell88j@gmail.com>
2022-01-01 01:25:34 +0000

    Client: Browse Request

---
+++ client/src/main/java/client/items/PriceAPIRequests.java
@@ -0,0 +1,23 @@
+package client.items;
+
+import client.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+public class PriceAPIRequests {
+
+    static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static Item[] browse(int pageNo, int maxPageLength, String priceApiHost, String priceApiPort) throws RestClientException {
+        logger.info("Browse| host: {}, port: {}, pageNo: {}, max: {}", priceApiHost, priceApiPort, pageNo, maxPageLength);
+        RestTemplate restTemplate = new RestTemplate();
+        Item[] items =
+                restTemplate.getForObject("http://localhost:8080/price/browse?page={pageNo}&max={maxPageLength}",
+                        Item[].class, pageNo, priceApiHost, priceApiPort, maxPageLength);
+
+        return items;
+    }
+
+}
