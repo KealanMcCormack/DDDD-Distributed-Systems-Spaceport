91f5fb8340141029e27959d907494464be729d9a
John <odonnell88j@gmail.com>
2022-01-01 00:07:11 +0000

    Client: Item Class

---
+++ client/src/main/java/client/items/Item.java
@@ -0,0 +1,34 @@
+package client.items;
+
+import java.io.Serializable;
+
+public class Item implements Serializable {
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+
+
