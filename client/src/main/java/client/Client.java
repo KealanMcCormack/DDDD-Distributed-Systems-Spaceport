d64424835be6afce5a5eea282e1de511d8bbfa2c
John <odonnell88j@gmail.com>
2022-01-01 01:26:05 +0000

    Client: Testing Main

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,8 +1,11 @@
 package client;
 
 
+import client.items.PriceAPIRequests;
+import client.items.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestClientException;
 
 
 /**
@@ -12,7 +15,25 @@ import org.slf4j.LoggerFactory;
  */
 public class Client {
 
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
+
     public static void main(String[] args) {
+        Item[] items;
+        try {
+
+
+            //items = Browse.browseAll("localhost", "8080");
+
+            items = PriceAPIRequests.browse(0, 20, "localhost", "8080");
+
+            for (Item item: items){
+                System.out.println(item);
+            }
+        }
+        catch (RestClientException e){
+            logger.error("Client| Exception| msg: {}, e: {}", e.getLocalizedMessage(), e.toString());
+
+        }
 
     }
 
