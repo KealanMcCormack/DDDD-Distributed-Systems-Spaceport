fefcd78b11c27d93b4e1867a606fb34e83315d25
John <odonnell88j@gmail.com>
2022-01-02 15:23:13 +0000

    Client: Price API request commenting

--- client/src/main/java/client/items/PriceAPIRequests.java
+++ client/src/main/java/client/items/PriceAPIRequests.java
@@ -3,32 +3,64 @@ package client.items;
 import client.Client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 public class PriceAPIRequests {
 
-    static Logger logger = LoggerFactory.getLogger(Client.class);
+    private static Logger logger = LoggerFactory.getLogger(PriceAPIRequests.class);
 
+
+    /**
+     * Price API Browse Paginated Request
+     *
+     * @param pageNo Page No.
+     * @param maxPageLength Max length of page
+     * @param priceApiHost Price API Host Address
+     * @param priceApiPort Price API Host Port
+     * @return Array of Items
+     * @throws RestClientException
+     */
     public static Item[] browse(int pageNo, int maxPageLength, String priceApiHost, String priceApiPort) throws RestClientException {
         logger.info("Browse| host: {}, port: {}, pageNo: {}, max: {}", priceApiHost, priceApiPort, pageNo, maxPageLength);
         RestTemplate restTemplate = new RestTemplate();
         Item[] items =
-                restTemplate.getForObject("http://localhost:8080/price/browse?page={pageNo}&max={maxPageLength}",
-                        Item[].class, pageNo, priceApiHost, priceApiPort, maxPageLength);
+                restTemplate.getForObject("http://{priceApiHost}:{priceApiPort}/price/browse?page={pageNo}&max={maxPageLength}",
+                        Item[].class, priceApiHost, priceApiPort,  pageNo, maxPageLength);
 
         return items;
     }
 
+    /**
+     * Price API Browse All Request
+     *
+     * @param priceApiHost Price API Host Address
+     * @param priceApiPort Price API Host Port
+     * @return Array of Items
+     * @throws RestClientException
+     */
     public static Item[] browseAll(String priceApiHost, String priceApiPort) throws RestClientException{
 
         logger.info("Browse All| host: {}, port: {}", priceApiHost, priceApiPort);
         RestTemplate restTemplate = new RestTemplate();
         Item[] items =
-                restTemplate.getForObject("http://{priceApiHost}:{priceApiPort}/price/browse/all", //"http://{priceApiHost}/price/browse/all",
+                restTemplate.getForObject("http://{priceApiHost}:{priceApiPort}/price/browse/all",
                         Item[].class, priceApiHost, priceApiPort);
 
+        logger.info("Browse All| noItems: {}, ", items.length);
+
         return items;
+    }
+
+
+    public static void add(Item item, String priceApiHost, String priceApiPort) throws RestClientException{
+        logger.info("Add Item| item:{}, host: {}, port: {}", item, priceApiHost, priceApiPort);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Item> request = new HttpEntity<>(item);
+
+        restTemplate.postForObject("http://{priceApiHost}:{priceApiPort}/price/add",
+                request, void.class, priceApiHost, priceApiPort);
     }
 
 }
