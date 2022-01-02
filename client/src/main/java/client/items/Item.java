afcabedddfb34ddf849ada9c748a07a5255a1b9a
John <odonnell88j@gmail.com>
2022-01-02 15:10:30 +0000

    Client: Item amount

--- client/src/main/java/client/items/Item.java
+++ client/src/main/java/client/items/Item.java
@@ -5,6 +5,37 @@ import java.io.Serializable;
 public class Item implements Serializable {
     private String name;
     private double price;
+    private double amount;
+
+    public Item(){
+        this.name = "";
+        this.price = -1;
+        this.amount = -1;
+    }
+
+    public Item(String name){
+        this.name = name;
+        this.price = -1;
+        this.amount = -1;
+    }
+
+    public Item(String name, double price){
+        this.name = name;
+        this.price = price;
+        this.amount = -1;
+    }
+
+    public Item(String name, int amount){
+        this.name = name;
+        this.price = -1;
+        this.amount = amount;
+    }
+
+    public Item(String name, double price, int amount){
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
 
     public String getName() {
         return name;
@@ -22,11 +53,20 @@ public class Item implements Serializable {
         this.price = price;
     }
 
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
     @Override
     public String toString() {
         return "Item{" +
                 "name='" + name + '\'' +
                 ", price=" + price +
+                ", amount=" + amount +
                 '}';
     }
 }
