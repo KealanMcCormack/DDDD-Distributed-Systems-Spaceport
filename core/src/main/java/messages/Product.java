864f4ab5aa6f7b5c4a96af9b44d1d052d18fd43c
KealanMcCormack <kealan.mccormack@ucdconnect.ie>
2022-01-03 13:11:11 +0000

    Added lombok constructor, getters and setters to product

--- core/src/main/java/messages/Product.java
+++ core/src/main/java/messages/Product.java
@@ -1,5 +1,14 @@
 package messages;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     String name;
     double amount;
