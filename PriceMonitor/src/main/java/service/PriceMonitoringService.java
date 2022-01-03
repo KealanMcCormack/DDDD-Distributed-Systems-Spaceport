641abd044144be2dd17deedb10e5bc778065011a
KealanMcCormack <kealan.mccormack@ucdconnect.ie>
2022-01-03 14:26:07 +0000

    Logic for price monitoring

--- PriceMonitor/src/main/java/service/PriceMonitoringService.java
+++ PriceMonitor/src/main/java/service/PriceMonitoringService.java
@@ -2,27 +2,71 @@ package service;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import messages.PriceItem;
 import messages.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class PriceMonitoringService {
     RestTemplate restTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(PriceMonitoringService.class);
 
     public void priceChange(Product product){
-        //restTemplate.getForObject("http://localhost:8081/inventory/" + product.getName(), )
+        Double amount = restTemplate.getForObject("http://localhost:8081/inventory/" + product.getName(), Double.class);
+
+        if(amount == null || amount == -1.0) {
+            logger.warn("Amount returned was : " + amount + " , stopping action");
+            return;
+        }
+
+        Double price = restTemplate.getForObject("http://localhost:8081/price/" + product.getName(), Double.class);
+
+        if(price == null || price == -1.0) {
+            logger.warn("Price returned was : " + price + " , stopping action");
+            return;
+        }
+
+        double change = percentageCalc(amount, product.getAmount(), price);
+
+        PriceItem priceItem = new PriceItem(product.getName(), change);
         if(product.getAmount() > 0){
-            decreasePrice();
+            decreasePrice(priceItem);
         }else{
-            increasePrice();
+            increasePrice(priceItem);
         }
     }
 
-    private void decreasePrice(){
+    private double percentageCalc(double inventoryAmount, double productAmount, double price){
+        double percentage = productAmount / inventoryAmount;
 
+        logger.info("Percentage price change calculated : " + percentage);
+
+        if(percentage >= 9.5){
+            return price * .095;
+        }
+
+        if(percentage < 0){
+            return 0;
+        }
+
+        return percentage * price;
     }
 
-    private void increasePrice(){
+    private void decreasePrice(PriceItem priceItem){
+        priceItem.setPrice(priceItem.getPrice() * -1.0);
+        logger.info("Decreasing price by : " + priceItem.getPrice());
+        final String uri = "http://localhost:8080/price/update";
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject(uri, priceItem, String.class);
+    }
 
+    private void increasePrice(PriceItem priceItem){
+        final String uri = "http://localhost:8080/price/update";
+        logger.info("Increasing price by : " + priceItem.getPrice());
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject(uri, priceItem, String.class);
     }
 }
---
+++ core/src/main/java/messages/PriceItem.java
@@ -0,0 +1,17 @@
+package messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PriceItem implements Serializable {
+    private String name;
+    private double price;
+}
