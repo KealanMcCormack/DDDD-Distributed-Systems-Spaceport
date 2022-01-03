d9025ad0d941776353f04420910bf21b951c34c3
KealanMcCormack <kealan.mccormack@ucdconnect.ie>
2022-01-03 14:32:58 +0000

    Added Actor messaging logic

--- PriceMonitor/src/main/java/actor/Monitor.java
+++ PriceMonitor/src/main/java/actor/Monitor.java
@@ -1,11 +1,27 @@
 package actor;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorSelection;
+import akka.actor.ActorSystem;
+import messages.Init;
+import messages.OrderRequest;
+import messages.Product;
+import service.PriceMonitoringService;
 
 public class Monitor extends AbstractActor {
-
+    PriceMonitoringService priceMonitoringService;
+    
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match(Init.class,
+                        msg -> {
+                            priceMonitoringService = msg.getService();
+                        })
+                .match(Product.class,
+                        msg -> {
+                            priceMonitoringService.priceChange(msg);
+                        })
+                .build();
     }
 }
