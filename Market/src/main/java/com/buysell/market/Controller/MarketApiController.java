56238517da841eabded086213416c3221e17f3b6
John <odonnell88j@gmail.com>
2022-01-03 16:10:42 +0000

    Market: Buy Error and code tidy, logging

--- Market/src/main/java/com/buysell/market/Controller/MarketApiController.java
+++ Market/src/main/java/com/buysell/market/Controller/MarketApiController.java
@@ -4,8 +4,11 @@ import com.buysell.market.DataObjects.Item;
 import com.buysell.market.DataObjects.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Controller for the Market API used for handling the buy and sell requests and creating orders
@@ -15,7 +18,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/market")
 public class MarketApiController {
-    final Logger logger = LoggerFactory.getLogger(MarketApiController.class);
+    private final Logger logger = LoggerFactory.getLogger(MarketApiController.class);
 
     //Notes
     // Careful about localhosts - for the moment Price is 8080, inventory is 8081
@@ -29,35 +32,56 @@ public class MarketApiController {
     @PostMapping("/buy")
     String buyItem(@RequestBody Item item, @RequestBody int customerID) {
 
-        if(item.getName() != null || item.getName().isEmpty()) {
-            logger.info("Requested item : " + item + " is null");
-            return "There seems to be something wrong with the name";
+        if(item == null) {
+            logger.info("Market Buy| Requested item is null");
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Invalid Item: null"
+            );
         }
-
-        if(itemAmount(item.getName()) < 0.0){
-            logger.warn("Requested item : " + item + " doesn't exist in inventory");
-            return "This item isn't in stock at the moment";
+        if(item.getName() == null || item.getName().isEmpty()) {
+            logger.info("Market Buy| Requested item: {} is null", item.getName());
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Invalid Item name: \"" + item.getName() + "\""
+            );
+        }
+        if(item.getAmount() <= 0.0){
+            logger.info("Market Buy| Requested item amount: {} is invalid", item.getAmount());
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Invalid Item amount: " + item.getAmount()
+            );
         }
 
         double amount = itemAmount(item.getName());
-        if(amount < 0.0 ) {
-            logger.info("Requested item : " + item + " doesn't have required stocks");
-            return "Sorry to say we ain't got enough a dis";
+        if(itemAmount(item.getName()) < 0.0){
+            logger.info("Market Buy| Requested item : {} doesn't exist in inventory", item.getName());
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Item Not Found: Item not carried Inventory: \"" + item.getName() + "\""
+            );
+        }
+
+
+        if(amount < item.getAmount()) {
+            logger.info("Market Buy| Requested item : Stock to Low: \"{}\", Requested Amount: {}, Inventory Amount: {}",
+                    item.getName(), item.getAmount(), amount);
+            throw new ResponseStatusException(
+                    HttpStatus.UNPROCESSABLE_ENTITY, "Purchase Not Processed: Stock to Low: \"" + item.getName() + "\", Requested Amount: "
+                    + item.getAmount() + ", Inventory Amount: " + amount
+            );
         }
 
         // Calculate total price
         double price = itemPrice(item.getName());
         if(price < 0.0){
-            logger.warn("Requested item : " + item + " had a price returned < 0");
-            return("Sorry, we couldn't find a price for that item. Please check back later");
+            logger.warn("Market Buy| Requested item : {}, no price available", item.getName());
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Item Not Found: No Price available: \"" + item.getName() + "\""
+            );
         }
 
         //Updating inventory database
         item.setAmount(item.getAmount()*-1);
 
-        final String uri = "http://localhost:8081/inventory/update";
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject(uri, item, String.class);
+        itemAmountUpdate(item);
 
         double totalCost = price * amount;
 
@@ -109,22 +133,25 @@ public class MarketApiController {
      * @return double price
      */
 
-    private static double itemPrice(String itemName)
-    {
-        final String uri = "http://localhost:8080/price/" + itemName;
+    private static double itemPrice(String itemName) throws ResponseStatusException{
+        Logger logger = LoggerFactory.getLogger(MarketApiController.class);
 
-        RestTemplate restTemplate = new RestTemplate();
-        double price = -1.0;
-        String result = restTemplate.getForObject(uri, String.class);
+        try {
+            logger.info("Market Item Amount| item: {}", itemName);
+            RestTemplate restTemplate = new RestTemplate();
+            Double price = restTemplate.getForObject("http://localhost:8080/price/{itemName}", Double.class, itemName);
 
-        try{
-            assert result != null;
-            price = Double.parseDouble(result);
-        } catch(Exception e){
-            System.out.println("Item Pricing failed : " + e);
+            if (price == null){
+                price = -1.0;
+            }
+            return price;
+
+        } catch(RestClientException e){
+            logger.error("Market Item Amount| RestClientException: {}", e.toString());
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error", e
+            );
         }
-
-        return price;
     }
 
     /**
@@ -132,23 +159,46 @@ public class MarketApiController {
      * @param itemName
      * @return double amount
      */
+    private static double itemAmount(String itemName)  throws ResponseStatusException{
+        Logger logger = LoggerFactory.getLogger(MarketApiController.class);
 
-    private static double itemAmount(String itemName)
-    {
-        final String uri = "http://localhost:8081/inventory/" + itemName;
+        try {
+            logger.info("Market Item Amount| item: {}", itemName);
+            RestTemplate restTemplate = new RestTemplate();
+            Double amount = restTemplate.getForObject("http://localhost:8081/inventory/{itemName}", Double.class, itemName);
 
-        RestTemplate restTemplate = new RestTemplate();
-        double amount = -1.0;
-        String result = restTemplate.getForObject(uri, String.class);
+            if (amount == null){
+                amount = -1.0;
+            }
+            return amount;
 
-        try{
-            assert result != null;
-            amount = Double.parseDouble(result);
-        } catch(Exception e){
-            System.out.println("Item Amount failed : " + e);
+        } catch(RestClientException e){
+            logger.error("Market Item Amount| RestClientException: {}", e.toString());
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error", e
+            );
         }
 
-        return amount;
+
+    }
+
+    private static void itemAmountUpdate(Item item)  throws ResponseStatusException{
+        Logger logger = LoggerFactory.getLogger(MarketApiController.class);
+
+        try {
+            logger.info("Market Item Amount Update| item: {}", item);
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.postForObject("http://localhost:8081/inventory/update", item, String.class);
+
+
+        } catch(RestClientException e){
+            logger.error("Market Item Amount Update| RestClientException: {}", e.toString());
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error", e
+            );
+        }
+
+
     }
 
 
