18f9d3ca2511d4be89d970339ec28d7f818e377f
John <odonnell88j@gmail.com>
2022-01-04 00:16:12 +0000

    Client: URI info updates to requests

--- client/src/main/java/client/items/InventoryAPIRequests.java
+++ client/src/main/java/client/items/InventoryAPIRequests.java
@@ -1,6 +1,7 @@
 package client.items;
 
 import client.exceptions.InvalidResponseException;
+import client.uri.UriInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestClientException;
@@ -8,52 +9,48 @@ import org.springframework.web.client.RestTemplate;
 
 public class InventoryAPIRequests {
 
-    private static Logger logger = LoggerFactory.getLogger(InventoryAPIRequests.class);
+    private final static Logger logger = LoggerFactory.getLogger(InventoryAPIRequests.class);
 
     /**
      * Inventory API Amount Request
      *
      * @param item Item to get inventory amount
-     * @param inventoryApiHost Inventory API Host Address
-     * @param inventoryApiPort Inventory API Host Port
+     * @param uriInfo URI info for API
      * @return Array of Items
      * @throws RestClientException
      * @throws InvalidResponseException
      */
-    public static Item itemAmount(Item item, String inventoryApiHost, String inventoryApiPort)
+    public static double itemAmount(Item item, UriInfo uriInfo)
             throws RestClientException, InvalidResponseException {
 
-        logger.info("Item Amount| host: {}, port: {}, item: {}", inventoryApiHost, inventoryApiPort, item);
+        logger.info("Item Amount| uri: {}, item: {}", uriInfo, item);
         RestTemplate restTemplate = new RestTemplate();
 
         Double amount =
                 restTemplate.getForObject("http://{inventoryApiHost}:{inventoryApiPort}/inventory/{}}",
-                        Double.class, inventoryApiHost, inventoryApiPort, item.getName());
+                        Double.class, uriInfo.getHost(), uriInfo.getPort(), item.getName());
 
         if (amount == null){
             throw new InvalidResponseException("Item Amount Response Null");
         }
-        else {
-            item.setAmount(amount);
-        }
-        return item;
+
+        return amount;
     }
 
     /**
      * Inventory API Item All Request
      *
-     * @param inventoryApiHost Inventory API Host Address
-     * @param inventoryApiPort Inventory API Host Port
+     * @param uriInfo URI info for API
      * @return Array of Items
      * @throws RestClientException
      */
-    public static Item[] itemAmountsAll(String inventoryApiHost, String inventoryApiPort) throws RestClientException{
+    public static Item[] itemAmountsAll(UriInfo uriInfo) throws RestClientException{
 
-        logger.info("Item Amount All| host: {}, port: {}", inventoryApiHost, inventoryApiPort);
+        logger.info("Item Amount All| uri: {}", uriInfo);
         RestTemplate restTemplate = new RestTemplate();
         Item[] items =
                 restTemplate.getForObject("http://{inventoryApiHost}:{inventoryApiPort}/price/browse/all",
-                        Item[].class, inventoryApiHost, inventoryApiPort);
+                        Item[].class, uriInfo.getHost(), uriInfo.getPort());
 
         logger.info("Item Amount All| noItems: {}", items.length);
 
@@ -64,18 +61,17 @@ public class InventoryAPIRequests {
      * Inventory API Amount Request on List of Items
      *
      * @param items Items to get inventory amount
-     * @param inventoryApiHost Inventory API Host Address
-     * @param inventoryApiPort Inventory API Host Port
+     * @param uriInfo URI info for API
      * @return Array of Items
      * @throws RestClientException
      * @throws InvalidResponseException
      */
-    public static Item[] itemAmountsList(Item[] items, String inventoryApiHost, String inventoryApiPort)
+    public static Item[] itemAmountsList(Item[] items, UriInfo uriInfo)
             throws RestClientException, InvalidResponseException {
 
-        logger.info("Item Amount List| host: {}, port: {}, items size: {}", inventoryApiHost, inventoryApiPort, items.length);
+        logger.info("Item Amount List| uri: {}, items size: {}", uriInfo, items.length);
         for (int i = 0; i < items.length; i++) {
-            items[i] = itemAmount(items[i], inventoryApiHost, inventoryApiPort);
+            items[i].setAmount(itemAmount(items[i], uriInfo));
         }
 
         return items;
@@ -86,18 +82,17 @@ public class InventoryAPIRequests {
      *
      * @param item Items to get inventory amount
      * @param amount Amount to the item to check
-     * @param inventoryApiHost Inventory API Host Address
-     * @param inventoryApiPort Inventory API Host Port
+     * @param uriInfo URI info for API
      * @return Array of Items
      * @throws RestClientException
      * @throws InvalidResponseException
      */
-    public static boolean hasItemAmount(Item item, double amount, String inventoryApiHost, String inventoryApiPort)
+    public static boolean hasItemAmount(Item item, double amount, UriInfo uriInfo)
             throws RestClientException, InvalidResponseException {
-        logger.info("Has Item Amount| host: {}, port: {}, items: {}, amount: {}", inventoryApiHost, inventoryApiPort, item, amount);
-        item = itemAmount(item, inventoryApiHost, inventoryApiPort);
+        logger.info("Has Item Amount| uri: {}, items: {}, amount: {}", uriInfo, item, amount);
+        double responseAmount = itemAmount(item, uriInfo);
 
-        return item.getAmount() >= amount;
+        return responseAmount >= amount;
     }
 
 }
--- client/src/main/java/client/market/MarketAPIRequests.java
+++ client/src/main/java/client/market/MarketAPIRequests.java
@@ -1,6 +1,7 @@
 package client.market;
 
 import client.items.Item;
+import client.uri.UriInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
@@ -9,27 +10,27 @@ import org.springframework.web.client.RestTemplate;
 
 public class MarketAPIRequests {
 
-    private static Logger logger = LoggerFactory.getLogger(MarketAPIRequests.class);
+    private final static Logger logger = LoggerFactory.getLogger(MarketAPIRequests.class);
 
-    public static void buy(String itemName, double amount, String id, String marketApiHost, String marketApiPort)
+    public static void buy(String itemName, double amount, String id, UriInfo uriInfo)
             throws RestClientException{
-        logger.info("Market Buy| host: {}, port: {}, item: {}, amount: {}, ID{}", marketApiHost, marketApiPort, itemName, amount, id);
+        logger.info("Market Buy| uri: {}, item: {}, amount: {}, ID{}", uriInfo, itemName, amount, id);
 
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<Item> request = new HttpEntity<>(new Item(itemName, -1, amount));
 
         restTemplate.postForObject("http://{priceApiHost}:{priceApiPort}/market/buy/{id}",
-                request, String.class, marketApiHost, marketApiPort, id);
+                request, String.class, uriInfo.getHost(), uriInfo.getPort(), id);
     }
 
-    public static void sell(String itemName, double amount, String id, String marketApiHost, String marketApiPort)
+    public static void sell(String itemName, double amount, String id, UriInfo uriInfo)
             throws RestClientException{
-        logger.info("Market Sell| host: {}, port: {}, item: {}, amount: {}, ID{}", marketApiHost, marketApiPort, itemName, amount, id);
+        logger.info("Market Sell| uri: {}, item: {}, amount: {}, ID{}", uriInfo, itemName, amount, id);
 
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<Item> request = new HttpEntity<>(new Item(itemName, -1, amount));
 
         restTemplate.postForObject("http://{priceApiHost}:{priceApiPort}/market/sell/{id}",
-                request, String.class, marketApiHost, marketApiPort, id);
+                request, String.class, uriInfo.getHost(), uriInfo.getPort(), id);
     }
 }
