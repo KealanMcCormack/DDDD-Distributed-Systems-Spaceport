9b7cd50828eb0a4655e995f239f689c4b07888d0
John <odonnell88j@gmail.com>
2022-01-04 04:35:57 +0000

    SpaceBank: Bank messages

---
+++ core/src/main/java/messages/BankResponse.java
@@ -0,0 +1,25 @@
+package messages;
+
+
+
+import core.BankStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class BankResponse implements Serializable {
+    private String receiverId;
+    private double charge;
+    private String payeeID;
+    private String bankTransactionID;
+    private BankStatus bankTransactionStatus;
+}
